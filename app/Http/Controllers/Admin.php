fc4c27f0743b6d96ac660d8a7145c510a0bcafec
Akhil <akhilk1904@gmail.com>
2025-06-23 19:26:53 +0530

    Dashboard grids

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bed;
+use App\Models\Harvest;
 use Illuminate\Http\Request;
 
 class Admin extends Controller
 {
     public function home(Request $request)
     {
-        return view('admin.home');
+        $total_beds = Bed::sum('no_of_beds');
+        $total_harvest = Harvest::sum('total_harvest_quantity');
+        return view('admin.home', compact('total_beds', 'total_harvest'));
     }
 }
