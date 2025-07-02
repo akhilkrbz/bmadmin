c17c98e6747dab030c11e83414c41f3066c33c55
Akhil <akhilk1904@gmail.com>
2025-07-03 01:07:35 +0530

    Update

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -6,6 +6,7 @@ use App\Models\Bed;
 use App\Models\Ledger;
 use App\Models\Harvest;
 use App\Models\PaymentVoucher;
+use App\Models\Receipt;
 use App\Models\Sale;
 use Illuminate\Http\Request;
 
@@ -18,6 +19,15 @@ class Admin extends Controller
         $monthly_harvest = Harvest::whereMonth('harvest_date', now()->month)->sum('total_harvest_quantity');
         $monthly_expenses = PaymentVoucher::whereMonth('voucher_date', now()->month)->sum('amount');
         $monthly_sales = Sale::whereMonth('sale_date', now()->month)->sum('total_amount');
-        return view('admin.home', compact('total_beds', 'monthly_harvest', 'monthly_expenses', 'monthly_sales'));
+        $amount_in_cash = Sale::where('payment_mode', 'cash')->sum('total_amount') + Receipt::where('payment_mode', 'cash')->sum('amount');
+        $amount_in_bank = Sale::where('payment_mode', 'bank')->sum('total_amount') + Receipt::where('payment_mode', 'bank')->sum('amount');
+        return view('admin.home', compact(
+            'total_beds', 
+            'monthly_harvest', 
+            'monthly_expenses', 
+            'monthly_sales',
+            'amount_in_cash',
+            'amount_in_bank'
+        ));
     }
 }
